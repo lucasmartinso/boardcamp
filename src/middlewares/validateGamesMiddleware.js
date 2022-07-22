4a4596fd76395446ad93ad8936fc21fc78685f10
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-07-22 17:42:40 -0300

    post e get ok

---
+++ src/middlewares/validateGamesMiddleware.js
@@ -0,0 +1,22 @@
+import connection from "../dbStrategy/postgresdb.js";
+import gameSchema from "../schemas/gameSchema.js";
+
+export async function validateGames(req,res,next) { 
+    const { name, categoryId, stockTotal, pricePerDay } = req.body;
+    const validation = gameSchema.validate(req.body); 
+    const findExist = await connection.query('SELECT * FROM categories WHERE id= $1',[categoryId]);
+
+    if(validation.error || !findExist || stockTotal<=0 || pricePerDay<=0) { 
+        return res.sendStatus(400);
+    }
+
+    try {
+        const findRepetead = await connection.query('SELECT * FROM games WHERE name= $1',[name]); 
+        if(findRepetead) { 
+            return res.sendStatus(409);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    next();
+}
--- src/routes/gameInfoRouter.js
+++ src/routes/gameInfoRouter.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { getCategories, getGames, postCategories, postGames } from "../controllers/gameInfoController.js";
 import { validateCategorie } from "../middlewares/validateCategorieMiddleware.js";
+import { validateGames } from "../middlewares/validateGamesMiddleware.js";
 
 const router = Router();
 
 router.get("/categories", getCategories); 
 router.post("/categories",validateCategorie,postCategories);
 router.get("/games",getGames); 
-router.post("/games",postGames);
+router.post("/games",validateGames,postGames);
 
 export default router;
 
